d07fc227cfbe6046c38286b03c81f8c314e7f83a
Anik Shakya <aniklinkin@gmail.com>
2023-09-26 08:02:27 +0545

    [Fixed:: Room database kapt implemented]

--- app/src/main/java/com/ismt/suitcase/room/Product.kt
+++ app/src/main/java/com/ismt/suitcase/room/Product.kt
@@ -7,13 +7,14 @@ import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "product")
+@Parcelize
 data class Product(
     val title: String,
     val price: String,
     val description: String,
     val image: String?,
     val location: String?
-) {
+) : Parcelable {
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 }
