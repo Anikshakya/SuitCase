b3efe93b4e67c5a3b73682e7485a161853cd12fe
Anik Shakya <aniklinkin@gmail.com>
2023-10-06 15:26:49 +0545

    [Implemented product share through sms]

--- app/src/main/java/com/ismt/suitcase/view/home/ProductDetailActivity.kt
+++ app/src/main/java/com/ismt/suitcase/view/home/ProductDetailActivity.kt
@@ -1,15 +1,27 @@
 package com.ismt.suitcase.view.home
 
+import android.Manifest
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.telephony.SmsManager
 import android.view.KeyEvent
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.ismt.suitcase.R
 import com.ismt.suitcase.constants.AppConstants
 import com.ismt.suitcase.databinding.ActivityProductDetailBinding
@@ -34,6 +46,7 @@ class ProductDetailActivity : AppCompatActivity() {
 
     companion object {
         const val RESULT_CODE_REFRESH = 2001
+        const val SMS_PERMISSIONS_REQUEST_CODE = 111
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,6 +63,29 @@ class ProductDetailActivity : AppCompatActivity() {
         // Set the buttons functionality in product details page
         setUpButtons()
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == SMS_PERMISSIONS_REQUEST_CODE) {
+            var allPermissionsGranted = true
+            for (result in grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    allPermissionsGranted = false
+                    break
+                }
+            }
+            if (allPermissionsGranted) {
+                showSmsBottomSheetDialog()
+            } else {
+                ToastUtils.showToast(this, "SMS permission not granted")
+            }
+        }
+    }
+
 
     // Populate
     private fun populateDataToTheViews(product: Product?) {
@@ -83,6 +119,7 @@ class ProductDetailActivity : AppCompatActivity() {
         setUpBackButton()
         setUpEditButton()
         setUpDeleteButton()
+        setUpShareButton()
     }
 
     // Back Button Behavior
@@ -92,6 +129,7 @@ class ProductDetailActivity : AppCompatActivity() {
         }
     }
 
+    // Set Up Edit Button
     private fun setUpEditButton() {
         detailViewBinding.ibEdit.setOnClickListener {
             val intent = Intent(this, AddOrUpdateActivity::class.java).apply {
@@ -101,6 +139,7 @@ class ProductDetailActivity : AppCompatActivity() {
         }
     }
 
+    // Set Up Delete Button
     private fun setUpDeleteButton() {
         detailViewBinding.ibDelete.setOnClickListener {
             MaterialAlertDialogBuilder(this)
@@ -143,6 +182,101 @@ class ProductDetailActivity : AppCompatActivity() {
                 }
             }
         }.start()
+    }
+
+    // Set up the sms button
+    private fun setUpShareButton() {
+        detailViewBinding.ibShare.setOnClickListener {
+            showSmsBottomSheetDialog()
+//            if (areSmsPermissionsGranted(this)) {
+//                showSmsBottomSheetDialog()
+//            } else {
+//                requestPermissions(
+//                    smsPermissionsList()!!.toTypedArray(),
+//                    SMS_PERMISSIONS_REQUEST_CODE
+//                )
+//            }
+        }
+    }
+
+    // Check for sms permission
+    private fun areSmsPermissionsGranted(context: Context): Boolean {
+        val sendSmsPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS)
+        val readSmsPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_SMS)
+        return sendSmsPermission == PackageManager.PERMISSION_GRANTED && readSmsPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun smsPermissionsList(): List<String>? {
+        val smsPermissions: MutableList<String> = ArrayList()
+        smsPermissions.add(Manifest.permission.READ_SMS)
+        smsPermissions.add(Manifest.permission.SEND_SMS)
+        return smsPermissions
+    }
+
+    // Display sms bottom sheet
+    private fun showSmsBottomSheetDialog() {
+        //TODO
+        val smsBottomSheetDialog = BottomSheetDialog(this)
+        smsBottomSheetDialog.setContentView(R.layout.bottom_sheet_send_sms)
+
+        val tilContact: TextInputLayout? = smsBottomSheetDialog.findViewById(R.id.til_contact)
+        val tieContact: TextInputEditText? = smsBottomSheetDialog.findViewById(R.id.tie_contact)
+        val sendSmsButton: MaterialButton? = smsBottomSheetDialog.findViewById(R.id.mb_send_sms)
+
+        tilContact?.setEndIconOnClickListener {
+            //TODO open Contact Activity
+        }
+
+        sendSmsButton?.setOnClickListener {
+            val contact = tieContact?.text.toString()
+            //TODO validation
+            if (contact.isBlank()) {
+                tilContact?.error = "Enter Contact"
+            } else {
+                sendSms(contact)
+            }
+        }
+
+        smsBottomSheetDialog.show()
+    }
+
+    private fun sendSms(contact: String) {
+        val smsManager: SmsManager = SmsManager.getDefault()
+        val description = if (product!!.description.length > 50) {
+            product!!.description.substring(0, 50)
+        } else {
+            product!!.description
+        }
+
+        val message = """
+        Item: ${product!!.title}
+        Price: ${product!!.price}
+        Description: $description
+        """.trimIndent()
+//        smsManager.sendTextMessage(
+//            contact,
+//            null,
+//            message,
+//            null,
+//            null
+//        )
+
+        // If the above SMS manager didn't work
+        openSmsAppToSendMessage(contact, message)
+    }
+
+
+    private fun openSmsAppToSendMessage(contact: String, message: String) {
+        val smsUri = Uri.parse("smsto:$contact")
+        val intent = Intent(Intent.ACTION_SENDTO, smsUri)
+        intent.putExtra("sms_body", message)
+
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            // Handle the case where no SMS app is available
+            Toast.makeText(this, "No SMS app found", Toast.LENGTH_SHORT).show()
+        }
     }
 
     // Set the result with finish
